2c4dc162197934be989a4ca5c4ea4b4f5b23acb0
Jens Brimberg <jens.brimberg@gmail.com>
2016-06-22 18:24:05 +0200

    start.spring.io started project

---
+++ src/main/java/se/jensim/streamlibrary/StreamLibraryApplication.java
@@ -0,0 +1,12 @@
+package se.jensim.streamlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamLibraryApplication.class, args);
+	}
+}
